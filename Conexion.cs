36f2b1cd5145cf76f005e810fcbd873e604eb7b4
Sebastian De Jesus Germoso Marrero <sebastiangermoso01@gmail.com>
2025-12-27 20:29:52 -0400

    Change cadena variable to private access modifier

--- Conexion.cs
+++ Conexion.cs
@@ -8,7 +8,7 @@ using Npgsql;
 
 public class Conexion
 {
-    public static string cadena =
+    private static string cadena =
         "Host=db.gcdmhkypzedogttworxr.supabase.co;Database=postgres;Username=postgres;Password=Germo0112200;SSL Mode=Require;Trust Server Certificate=true";
 
     public static NpgsqlConnection GetConexion()

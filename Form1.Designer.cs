de9f7221bf07a624f2db0b3fde9b9368ece1cec6
Oscar Mora <oscarmora190207@gmail.com>
2025-12-28 20:00:56 -0400

    CRUD de productos usando procedimientos almacenados y auditoría

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PDV_PRO3
 {
-    partial class Form1
+    partial class FrmProductos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,371 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCodigoBarra = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbInventario = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtIdProducto = new System.Windows.Forms.TextBox();
+            this.cmbImpuesto = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigoBarra
+            // 
+            this.txtCodigoBarra.Location = new System.Drawing.Point(85, 87);
+            this.txtCodigoBarra.Name = "txtCodigoBarra";
+            this.txtCodigoBarra.Size = new System.Drawing.Size(125, 20);
+            this.txtCodigoBarra.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(85, 122);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(216, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(325, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(366, 122);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(44, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Descripcion";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(38, 175);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(263, 77);
+            this.txtDescripcion.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(482, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Costo";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(523, 122);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(100, 20);
+            this.txtCosto.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(642, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Stock";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(683, 122);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 10;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(328, 187);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(138, 21);
+            this.cmbCategoria.TabIndex = 12;
+            // 
+            // cmbInventario
+            // 
+            this.cmbInventario.FormattingEnabled = true;
+            this.cmbInventario.Location = new System.Drawing.Point(485, 187);
+            this.cmbInventario.Name = "cmbInventario";
+            this.cmbInventario.Size = new System.Drawing.Size(138, 21);
+            this.cmbInventario.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.label7.Location = new System.Drawing.Point(325, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Categoria";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.label8.Location = new System.Drawing.Point(482, 168);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Inventario";
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProductos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(38, 272);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(929, 218);
+            this.dgvProductos.TabIndex = 16;
+            this.dgvProductos.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProductos_CellDoubleClick);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(867, 122);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.label9.Location = new System.Drawing.Point(803, 125);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 16);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Cantidad";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(328, 229);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 19;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(409, 229);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(78, 23);
+            this.btnEditar.TabIndex = 20;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnAgregarStock_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(500, 229);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 21;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(871, 234);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "ID";
+            this.label10.Visible = false;
+            // 
+            // txtIdProducto
+            // 
+            this.txtIdProducto.Location = new System.Drawing.Point(921, 231);
+            this.txtIdProducto.Name = "txtIdProducto";
+            this.txtIdProducto.ReadOnly = true;
+            this.txtIdProducto.Size = new System.Drawing.Size(46, 20);
+            this.txtIdProducto.TabIndex = 22;
+            this.txtIdProducto.Visible = false;
+            // 
+            // cmbImpuesto
+            // 
+            this.cmbImpuesto.FormattingEnabled = true;
+            this.cmbImpuesto.Location = new System.Drawing.Point(645, 187);
+            this.cmbImpuesto.Name = "cmbImpuesto";
+            this.cmbImpuesto.Size = new System.Drawing.Size(138, 21);
+            this.cmbImpuesto.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.label11.Location = new System.Drawing.Point(642, 167);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 16);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Impuestos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(586, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "ENTRADA STOCK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAgregarStock_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(705, 229);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(78, 23);
+            this.btnNuevo.TabIndex = 27;
+            this.btnNuevo.Text = "LIMPIAR";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label12.Location = new System.Drawing.Point(33, 29);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(485, 30);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "PANEL ADMINISTRADOR DE PRODUCTOS";
+            // 
+            // FrmProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1010, 511);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.cmbImpuesto);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtIdProducto);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbInventario);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigoBarra);
+            this.Name = "FrmProductos";
+            this.Text = "Administrador de Productos";
+            this.Load += new System.EventHandler(this.FrmProductos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCodigoBarra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbInventario;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtIdProducto;
+        private System.Windows.Forms.ComboBox cmbImpuesto;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,211 @@ using System.Windows.Forms;
 
 namespace PDV_PRO3
 {
-    public partial class Form1 : Form
+    public partial class FrmProductos : Form
     {
-        public Form1()
+        ProductoDAO productoDAO = new ProductoDAO();
+        int idProductoSeleccionado = 0;
+        int usuarioActual = 1; // eto e temporal
+
+        public FrmProductos()
         {
             InitializeComponent();
+        }
+
+        private void FrmProductos_Load(object sender, EventArgs e)
+        {
+            CargarGrid();
+            CargarCombos();
+            LimpiarCampos();
+        }
+
+        // CARGAS INICIALES
+        private void CargarGrid()
+        {
+            dgvProductos.DataSource = productoDAO.ListarProductos();
+            dgvProductos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void CargarCombos()
+        {
+            cmbCategoria.DataSource = productoDAO.ListarCategorias();
+            cmbCategoria.DisplayMember = "nombre";
+            cmbCategoria.ValueMember = "id_categoria";
+            cmbCategoria.SelectedIndex = -1;
+
+            cmbInventario.DataSource = productoDAO.ListarInventarios();
+            cmbInventario.DisplayMember = "lugar";
+            cmbInventario.ValueMember = "id_inventario";
+            cmbInventario.SelectedIndex = -1;
+
+            cmbImpuesto.Items.Clear();
+            cmbImpuesto.Items.Add("Exento");
+            cmbImpuesto.Items.Add("Sujeto");
+            cmbImpuesto.SelectedIndex = -1;
+
+            dgvProductos.Columns["id_producto"].Visible = false;
+            dgvProductos.Columns["id_categoria"].Visible = false;
+            dgvProductos.Columns["id_inventario"].Visible = false;
+        }
+
+        // BOTÓN GUARDAR
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!ValidarFormulario()) return;
+
+                productoDAO.CrearProducto(
+                    txtCodigoBarra.Text.Trim(),
+                    txtNombre.Text.Trim(),
+                    txtDescripcion.Text.Trim(),
+                    decimal.Parse(txtPrecio.Text),
+                    decimal.Parse(txtCosto.Text),
+                    int.Parse(txtStock.Text),
+                    (int)cmbCategoria.SelectedValue,
+                    cmbImpuesto.Text,
+                    usuarioActual,
+                    "Stock inicial",
+                    (int)cmbInventario.SelectedValue
+                );
+
+                MessageBox.Show("Producto registrado correctamente");
+                CargarGrid();
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("productos_codigo_barra_key"))
+                {
+                    MessageBox.Show(
+                        "❌ Ya existe un producto con ese código de barra",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                    );
+                    txtCodigoBarra.Focus();
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        // DOBLE CLICK GRID
+        private void dgvProductos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvProductos.CurrentRow == null) return;
+
+            DataGridViewRow fila = dgvProductos.CurrentRow;
+
+            idProductoSeleccionado = Convert.ToInt32(fila.Cells["id_producto"].Value);
+
+            txtCodigoBarra.Text = fila.Cells["codigo_barra"].Value.ToString();
+            txtNombre.Text = fila.Cells["nombre"].Value.ToString();
+            txtDescripcion.Text = fila.Cells["descripcion"].Value.ToString();
+            txtPrecio.Text = fila.Cells["precio"].Value.ToString();
+            txtCosto.Text = fila.Cells["costo"].Value.ToString();
+            txtStock.Text = fila.Cells["stock"].Value.ToString();
+
+            cmbImpuesto.Text = fila.Cells["impuestos"].Value.ToString();
+
+            cmbCategoria.SelectedValue =
+                Convert.ToInt32(fila.Cells["id_categoria"].Value);
+
+            cmbInventario.SelectedValue =
+                Convert.ToInt32(fila.Cells["id_inventario"].Value);
+        }
+
+        // AGREGAR STOCK
+        private void btnAgregarStock_Click(object sender, EventArgs e)
+        {
+            if (idProductoSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un producto con doble click");
+                return;
+            }
+
+            if (!int.TryParse(txtCantidad.Text, out int cantidad) || cantidad <= 0)
+            {
+                MessageBox.Show("Cantidad inválida");
+                return;
+            }
+
+            productoDAO.AgregarStock(
+                idProductoSeleccionado,
+                cantidad,
+                "Reposición",
+                usuarioActual
+            );
+
+            MessageBox.Show("Stock actualizado");
+            CargarGrid();
+            txtCantidad.Clear();
+        }
+
+        // ELIMINAR
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (idProductoSeleccionado == 0)
+            {
+                MessageBox.Show("Seleccione un producto");
+                return;
+            }
+
+            productoDAO.EliminarProducto(idProductoSeleccionado);
+            MessageBox.Show("Producto eliminado correctamente");
+            CargarGrid();
+            LimpiarCampos();
+        }
+
+        // LIMPIAR
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtCodigoBarra.Clear();
+            txtNombre.Clear();
+            txtDescripcion.Clear();
+            txtPrecio.Clear();
+            txtCosto.Clear();
+            txtStock.Clear();
+            txtCantidad.Clear();
+
+            cmbCategoria.SelectedIndex = -1;
+            cmbInventario.SelectedIndex = -1;
+            cmbImpuesto.SelectedIndex = -1;
+
+            idProductoSeleccionado = 0;
+            txtCodigoBarra.Focus();
+        }
+
+        // VALIDACIONES
+        private bool ValidarFormulario()
+        {
+            if (txtNombre.Text.Trim() == "" ||
+                txtPrecio.Text.Trim() == "" ||
+                txtStock.Text.Trim() == "" ||
+                cmbCategoria.SelectedIndex == -1 ||
+                cmbInventario.SelectedIndex == -1 ||
+                cmbImpuesto.SelectedIndex == -1)
+            {
+                MessageBox.Show("Complete todos los campos obligatorios");
+                return false;
+            }
+
+            if (!decimal.TryParse(txtPrecio.Text, out _) ||
+                !decimal.TryParse(txtCosto.Text, out _) ||
+                !int.TryParse(txtStock.Text, out _))
+            {
+                MessageBox.Show("Formato numérico incorrecto");
+                return false;
+            }
+
+            return true;
         }
     }
 }

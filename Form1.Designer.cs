985f9bbb4f56f9e1c04228c21989325a526e4d86
DavidDj06 <daviddj060405@gmail.com>
2026-01-05 00:03:25 -0400

    Creación de l diseño y rama de Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -19,22 +19,125 @@
             }
             base.Dispose(disposing);
         }
-
-        #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.BtnIniciar_sesión = new System.Windows.Forms.Button();
+            this.TxtContraseña = new System.Windows.Forms.TextBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label4.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 56);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "👤";
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.Location = new System.Drawing.Point(173, 128);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(140, 35);
+            this.BtnSalir.TabIndex = 14;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = true;
+            // 
+            // BtnIniciar_sesión
+            // 
+            this.BtnIniciar_sesión.BackColor = System.Drawing.SystemColors.Control;
+            this.BtnIniciar_sesión.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.BtnIniciar_sesión.Location = new System.Drawing.Point(26, 128);
+            this.BtnIniciar_sesión.Name = "BtnIniciar_sesión";
+            this.BtnIniciar_sesión.Size = new System.Drawing.Size(140, 35);
+            this.BtnIniciar_sesión.TabIndex = 13;
+            this.BtnIniciar_sesión.Text = "Iniciar sesión";
+            this.BtnIniciar_sesión.UseVisualStyleBackColor = false;
+            this.BtnIniciar_sesión.Click += new System.EventHandler(this.BtnIniciar_sesión_Click);
+            // 
+            // TxtContraseña
+            // 
+            this.TxtContraseña.Location = new System.Drawing.Point(212, 84);
+            this.TxtContraseña.Name = "TxtContraseña";
+            this.TxtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.TxtContraseña.TabIndex = 12;
+            this.TxtContraseña.UseSystemPasswordChar = true;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(212, 58);
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.TxtUsuario.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(106, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Contraseña";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(106, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 21);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Sistema De Venta";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(342, 176);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnSalir);
+            this.Controls.Add(this.BtnIniciar_sesión);
+            this.Controls.Add(this.TxtContraseña);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
-        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Button BtnIniciar_sesión;
+        private System.Windows.Forms.TextBox TxtContraseña;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,41 @@ namespace PDV_PRO3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+      
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIniciar_sesión_Click(object sender, EventArgs e)
+        {
+            {
+                if (TxtUsuario.Text == "" || TxtContraseña.Text == "")
+                {
+                    MessageBox.Show("Complete todos los campos");
+                    return;
+                }
+                /*Form Formulario = new formMenuPrincipal();
+                Formulario.Show();
+                this.Hide();*/
+            }
+
+        }
     }
 }

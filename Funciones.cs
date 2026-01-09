828bd5a01d2594035f975a18c44867f0c5ccfc93
Sebastian Germoso <sebastiangermoso01@gmail.com>
2026-01-09 19:36:37 -0400

    agrega clase funciones

---
+++ Funciones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV_PRO3
+{
+    internal class Funciones
+    {
+    }
+}

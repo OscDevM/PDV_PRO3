2285fec1f48b0d4b09c5f87cc982f00b0fbdcbcf
Sebastian Germoso <sebastiangermoso01@gmail.com>
2026-01-10 03:25:24 -0400

    agrega select y codigo para pasar datos del dgv a los textbox

--- Categoria.cs
+++ Categoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,43 @@ namespace PDV_PRO3
         public Categoria()
         {
             InitializeComponent();
+        }
+
+        bool insertar = true;
+        int idCategoria;
+
+        private void Categoria_Load(object sender, EventArgs e)
+        {
+            using (var conn = Conexion.GetConexion())
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM categoria_productos ORDER BY id_categoria";
+                using (var da = new NpgsqlDataAdapter(sql, conn))
+                {
+
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvCategorias.DataSource = dt;
+                }
+            }
+        }
+
+        private void dgvCategorias_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            insertar = false;
+            idCategoria =Convert.ToInt32(dgvCategorias.CurrentRow.Cells["id_categoria"].Value);
+            txtNombre.Text = dgvCategorias.CurrentRow.Cells["nombre"].Value.ToString();
+            txtDescripcion.Text = dgvCategorias.CurrentRow.Cells["descripcion"].Value.ToString();
+
+            if(dgvCategorias.CurrentRow.Cells["activo"].Value.ToString() == "TRUE")
+            {
+                cbEstatus.SelectedIndex = 0;
+            }
+            else
+            {
+                cbEstatus.SelectedIndex = 1;
+            }
         }
     }
 }
